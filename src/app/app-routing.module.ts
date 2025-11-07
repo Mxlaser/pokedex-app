9e074568e8741c412a446a6fb50b630dff516d3a
Nikola.Z <nzivkovic@eig.fr>
2025-11-07 12:54:33 +0100

    création routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -6,7 +7,7 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PokemonDetailComponent } from './pages/pokemon-detail/pokemon-detail.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -14,3 +15,9 @@ export const routes: Routes = [
   { path: 'pokemon/:id', component: PokemonDetailComponent },
   { path: '**', component: NotFoundComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
