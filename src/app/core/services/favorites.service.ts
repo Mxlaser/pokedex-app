8f2ed771e1eb04c8d9e86b30c3e42b2a83865299
Nikola.Z <nzivkovic@eig.fr>
2025-11-14 10:44:40 +0100

    merge des deux branches

--- src/app/core/services/favorites.service.ts
+++ src/app/core/services/favorites.service.ts
@@ -1,45 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
-import { PokemonSummary } from '../models/pokemon-summary.model';
-import { PokemonDetail } from '../models/pokemon-detail.model';
+import { Favorite } from '../models/favorite';
+import { Observable, switchMap } from 'rxjs';
+
+const API = 'http://localhost:3000'; // json-server
 
 @Injectable({ providedIn: 'root' })
-export class PokemonService {
-  private readonly base = environment.apiPokeBase;
+export class FavoritesService {
+  private http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
-
-  // Gen 1 : 1–151
-  getFirstGen(): Observable<PokemonSummary[]> {
-    const url = `${this.base}/pokemon?limit=151&offset=0`;
-    return this.http.get<{ results: { name: string; url: string }[] }>(url).pipe(
-      map(res =>
-        res.results.map(r => ({
-          id: this.extractIdFromUrl(r.url),
-          name: r.name,
-        }))
-      )
-    );
+  getByUser(userId: number): Observable<Favorite[]> {
+    return this.http.get<Favorite[]>(`${API}/favorites`, {
+      params: { userId } as any,
+    });
   }
 
-  getById(id: number): Observable<PokemonDetail> {
-    return this.http.get<PokemonDetail>(`${this.base}/pokemon/${id}`);
+  add(userId: number, pokemonId: number) {
+    return this.http
+      .get<Favorite[]>(`${API}/favorites`, {
+        params: { userId, pokemonId } as any,
+      })
+      .pipe(
+        switchMap(list =>
+          list.length
+            ? this.http.delete(`${API}/favorites/${list[0].id}`)
+            : this.http.post<Favorite>(`${API}/favorites`, {
+                userId,
+                pokemonId,
+              })
+        )
+      );
   }
 
-  getTypes(): Observable<string[]> {
-    return this.http.get<{ results: { name: string }[] }>(`${this.base}/type`).pipe(
-      map(res =>
-        res.results
-          .map(t => t.name)
-          .filter(n => !['shadow', 'unknown'].includes(n))
-      )
-    );
-  }
-
-  private extractIdFromUrl(url: string): number {
-    const parts = url.split('/').filter(Boolean);
-    return Number(parts[parts.length - 1]);
+  remove(favoriteId: number) {
+    return this.http.delete(`${API}/favorites/${favoriteId}`);
   }
 }
